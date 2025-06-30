0015aa6c9f01e62b0e69a9f1e15f76632c7e1f8c
shivangam-git <shivangammahajan99@gmail.com>
2025-06-30 15:51:26 +0530

    bug fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import pic from '../assets/mypic.jpg'
+import pic from '../assets/myPic.jpg'
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from 'react-scroll'
 const Home = () => {
